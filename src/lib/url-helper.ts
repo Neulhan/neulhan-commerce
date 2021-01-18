f8cae5b1bff6da0206fb3ea529a1a983e1b4ec53
neulhan <sting7675@gmail.com>
2021-01-19 00:47:58 +0900

    remove package: remove auth0

--- src/lib/url-helper.ts
+++
@@ -1,7 +0,0 @@
-export default function createLoginUrl(redirectTo?: string): string {
-  console.log("CREATE LOGIN URL");
-  if (redirectTo) {
-    return `/api/login?redirectTo=${encodeURIComponent(redirectTo)}`;
-  }
-  return `/api/login`;
-}
