d0b8a5cd4436bccd1caa818c008317ecc8ff6acb
neulhan <sting7675@gmail.com>
2021-01-19 01:45:11 +0900

    remove code: remove unuse env

--- next.config.js
+++ next.config.js
@@ -27,9 +27,7 @@ module.exports = (phase, { defaultConfig }) => {
         return config;
       },
       env: {
-        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-        AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+        TEST: process.env.TEST,
       },
     };
   }
