760a79d0aa2254522c7009535c0f9787198f5f29
neulhan <sting7675@gmail.com>
2021-01-27 23:47:55 +0900

    create page: admin create post page

---
+++ src/pages/admin/product/create.tsx
@@ -0,0 +1,7 @@
+import AdminContainer from "@src/containers/AdminContainer";
+
+function ProductCreateAdmin() {
+  return <AdminContainer>PRODUCT CREATE!</AdminContainer>;
+}
+
+export default ProductCreateAdmin;
