8d9574cfe6378a160a7a3a5f13231bf645b530ce
neulhan <sting7675@gmail.com>
2021-01-29 10:21:19 +0900

    create container: add admin post container

---
+++ src/containers/AdminContainer/Product/Post/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { CreateProduct, Product, UpdateProduct } from "@src/@types/product";
+import Router from "next/router";
+import style from "./index.module.scss";
+
+interface Props {
+  product: Product | undefined;
+}
+
+function AdminProductPostContainer(props: Props) {
+  const { product: productDefault } = props;
+  const [product, setProduct] = productDefault
+    ? useState<Product>(productDefault)
+    : useState<Product>({});
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    product[e.target.name] = isNaN(+value) ? value : +value;
+    setProduct(product);
+  };
+
+  const handleClick = () => {
+    if (productDefault) {
+      UpdateProduct(product);
+    } else {
+      CreateProduct(product);
+    }
+    Router.push("/admin/product/list");
+  };
+
+  return (
+    <div className={style.container}>
+      <h1>{productDefault ? "상품수정" : "상품등록"}</h1>
+      <p>상품이름</p>
+      <input type="text" name="productName" onChange={handleChange} />
+      <p>사진</p>
+      <input type="text" name="img" onChange={handleChange} />
+      <p>사진 alt</p>
+      <input type="text" name="imgAlt" onChange={handleChange} />
+      <p>가격</p>
+      <input type="number" name="price" onChange={handleChange} />
+      <p>할인가</p>
+      <input type="number" name="promotion" onChange={handleChange} />
+      <p>상품설명</p>
+      <input type="text" name="desc" onChange={handleChange} />
+      <button onClick={handleClick}>등록하기</button>
+    </div>
+  );
+}
+export default AdminProductPostContainer;
