d55a97f931c75ef43332ea1ed026eb4d299aad9f
neulhan <sting7675@gmail.com>
2021-01-29 10:35:35 +0900

    update page: modify main landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@ import style from "./index.module.scss";
 import FirstNav from "@components/FirstNav";
 import SecondNav from "@components/SecondNav";
 import ThirdNav from "@components/ThirdNav";
+import { GetStaticProps } from "next";
+import { GetProducts } from "@src/@types/product";
 
-function HomePage() {
+function HomePage({ products }) {
+  console.log(products);
   return (
     <>
       <FirstNav></FirstNav>
@@ -14,11 +17,40 @@ function HomePage() {
           <h1 className={style.neulhan}>
             NEULHAN <span>COMMERCE</span>
           </h1>
-          <button className={style.button}>Let's Start!</button>
+        </div>
+      </div>
+      <div className={style.wrapper}>
+        <div className="container">
+          <ul className={style.cardWrapper}>
+            {products.map((p) => (
+              <li key={p.ID}>
+                <div className={style.card}>
+                  <div
+                    className={style.image}
+                    style={{ backgroundImage: `url(${p.img})` }}
+                  ></div>
+                  <p className={style.name}>{p.productName}</p>
+                  <p className={style.price}>{p.price.toLocaleString()}</p>
+                  <p className={style.promotion}>
+                    {p.promotion.toLocaleString()} KRW
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await GetProducts();
+  return {
+    props: {
+      products,
+    },
+  };
+};
 
 export default HomePage;
