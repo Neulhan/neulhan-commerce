341e21befc9f4fe2b741f2f4fac93a5353d682c6
neulhan <sting7675@gmail.com>
2021-01-30 18:33:30 +0900

    update component: change nav inner

--- src/components/SecondNav/index.tsx
+++ src/components/SecondNav/index.tsx
@@ -1,11 +1,21 @@
 import style from "./index.module.scss";
+import Link from "next/link";
 
 function SecondNavigationComponent() {
   return (
     <>
       <nav className={style.second}>
-        <div className={style.container}>
-          <div className={style.text + " container"}>NH</div>
+        <div className="container">
+          <div className={style.innerContainer}>
+            <Link href="/">
+              <a>
+                <img src="/logo.svg" alt="" />
+                <div className={style.text}>
+                  Neulhan <span>Commerce</span>
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
       </nav>
     </>
