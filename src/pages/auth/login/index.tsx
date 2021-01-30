dc95e417b78fda3aef371b61c4cb9bf5482d11af
neulhan <sting7675@gmail.com>
2021-01-30 22:06:09 +0900

    create page: create login page

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,12 @@
+import MainContainer from "@src/containers/MainContainer";
+import LoginContainer from "@src/containers/auth/LoginContainer";
+
+function LoginPage() {
+  return (
+    <MainContainer>
+      <LoginContainer></LoginContainer>
+    </MainContainer>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,12 @@
+import MainContainer from "@src/containers/MainContainer";
+import SignupContainer from "@src/containers/auth/SignupContainer";
+
+function SignupPage() {
+  return (
+    <MainContainer>
+      <SignupContainer></SignupContainer>
+    </MainContainer>
+  );
+}
+
+export default SignupPage;
