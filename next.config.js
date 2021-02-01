ae1add92da627583dba8636557a6179a10fb749f
neulhan <sting7675@gmail.com>
2021-02-01 22:21:27 +0900

    update env: add backend api url into env file

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ module.exports = (phase, { defaultConfig }) => {
       },
       env: {
         TEST: process.env.TEST,
+        API_URL: process.env.API_URL,
         KAKAO_TOKEN: process.env.KAKAO_TOKEN,
         GITHUB_TOKEN: process.env.GITHUB_TOKEN,
         APPLE_CLIENT_ID: process.env.APPLE_CLIENT_ID,
