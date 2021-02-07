4ba98c6d56aa693c79012dabbb147c7fc4e75426
neulhan <sting7675@gmail.com>
2021-02-07 16:00:43 +0900

    update app: add custom favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,10 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Neulhan Commerce</title>
+        <title>DEVGOOD | 개발자를 위한 온라인커머스</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
 
       <Component {...pageProps} />
